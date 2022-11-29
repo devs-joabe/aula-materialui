c8e121e55ccd650a24c7d65fa40ba287914687b0
devs-joabe~ <joabefnascimento1@outlook.com~>
2022-11-29 01:09:26 -0300

    feat: adicionado tema escuro e troca de theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import { AppThemeProvider } from "./shared/contexts";
 import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
+    <AppThemeProvider>
     <BrowserRouter>
    <AppRoutes/>
     </BrowserRouter>
+    </AppThemeProvider>
   );
 }
